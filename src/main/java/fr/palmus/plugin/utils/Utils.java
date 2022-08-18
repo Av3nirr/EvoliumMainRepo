54b3bd3a0fea2e6523ab83164b21e322cdd1ef0c
Arnaud MINE <arnaudmine29@gmail.com>
2022-08-18 13:24:37 +0200

    Fix rtp

--- src/main/java/fr/palmus/plugin/utils/Utils.java
+++ src/main/java/fr/palmus/plugin/utils/Utils.java
@@ -1,7 +1,5 @@
 package fr.palmus.plugin.utils;
 
 public class Utils {
-    public static int getRandomNumber(int min, int max){
-        return (int)Math.floor(Math.random()*(max-min+1)+min);
-    }
+
 }
