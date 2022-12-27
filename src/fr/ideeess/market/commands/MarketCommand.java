418152242da8e3053261f033eb9a1e898bf70032
IDEEESS <64473138+IDEEESS@users.noreply.github.com>
2022-12-27 01:31:58 +0100

    Problème 1.1

--- src/fr/ideeess/market/commands/MarketCommand.java
+++ src/fr/ideeess/market/commands/MarketCommand.java
@@ -3,6 +3,7 @@ package fr.ideeess.market.commands;
 import fr.ideeess.market.MarketEvolium;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,31 +29,30 @@ public class MarketCommand implements CommandExecutor {
 
                         if (!args[2].equalsIgnoreCase("")){
 
-                            if (player.hasPermission("market.sellitem")){
+                            if (player.hasPermission("market.sellitem")) {
 
-                                ItemStack it = player.getItemInHand();
+                                ItemStack it = player.getItemInUse();
                                 int itNumber = Integer.parseInt(args[1]);
                                 int itPrice = Integer.parseInt(args[2]);
+                                if (it != null || it.getType() != Material.AIR) {
+                                    if (player.getInventory().contains(it.getType(), itNumber)) {
+                                        int seconds = Math.toIntExact(System.currentTimeMillis() / 1000);
 
-                                if (player.getInventory().contains(it.getType(),itNumber)){
-                                    int seconds = Math.toIntExact(System.currentTimeMillis() / 1000);
+                                        main.getConfig().set(seconds + ".owner", player.getDisplayName());
+                                        main.getConfig().set(seconds + ".item.name", it.getItemMeta().getDisplayName());
+                                        main.getConfig().set(seconds + ".item.lore", it.getItemMeta().getLore());
+                                        main.getConfig().set(seconds + ".item.type", it.getType());
+                                        main.getConfig().set(seconds + ".price", itPrice);
+                                        main.getConfig().set(seconds + ".quantity", itNumber);
 
-                                    main.getConfig().set(seconds + ".owner",player.getDisplayName());
-                                    main.getConfig().set(seconds + ".item.name",it.getItemMeta().getDisplayName());
-                                    main.getConfig().set(seconds + ".item.lore",it.getItemMeta().getLore());
-                                    main.getConfig().set(seconds + ".item.type",it.getType());
-                                    main.getConfig().set(seconds + ".price",itPrice);
-                                    main.getConfig().set(seconds + ".quantity",itNumber);
-
-                                    player.sendMessage(ChatColor.GREEN + "Vous vendez à présent " + itNumber + " " + it.getItemMeta().getDisplayName() + " à " + itPrice +" EvoCoins");
-
-                                    main.saveConfig();
-
+                                        player.sendMessage(ChatColor.GREEN + "Vous vendez à présent " + itNumber + " " + it.getItemMeta().getDisplayName() + " à " + itPrice + " EvoCoins");
+                                        main.saveConfig();
+                                        return false;
+                                    }
+                                    player.sendMessage(ChatColor.RED + "Vous n'avez pas " + itNumber + " " + it.getItemMeta().getDisplayName());
                                     return false;
                                 }
-
-                                player.sendMessage(ChatColor.RED + "Vous n'avez pas " + itNumber + " " + it.getItemMeta().getDisplayName());
-
+                                player.sendMessage(ChatColor.RED + "Vous ne pouvez pas vendre de l'air");
                                 return false;
                             }
 
